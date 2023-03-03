ae69bcb0a3afc23d0fd583e51b322955dc55278d
brfft <belkacem.yerfa@gmail.com>
2023-03-03 11:24:41 +0100

    Setup client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <DataLayer initialState={initialState} reducer={reducer}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </DataLayer>
+);
