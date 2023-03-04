be587b666ff4eed49597c85fd429f4db4f93f3b7
abdoude12 <deabdou55@gmail.com>
2023-03-04 11:18:39 +0100

    fixing bugs

--- client/src/components/BodySection.jsx
+++ client/src/components/BodySection.jsx
@@ -4,7 +4,7 @@ import GroupeImage from "../assets/Group 2181.png";
 
 export const BodySection = () => {
   return (
-    <section className=" w-full mt-[4rem]" id="Home" >
+    <section className=" w-full mt-[4rem]" id="Home">
       <div className="max-w-[1360px] m-auto flex">
         <div className="flex  items-start justify-center flex-col gap-6 translate-x-10">
           <h1 className="text-[54px] min-w-fit font-bold">
@@ -13,11 +13,11 @@ export const BodySection = () => {
           </h1>
           <p className="text-[#414141] text-[20px] font-normal ">
             Empowering businesses with intuitive data <br /> visualization tools
-            to unlock valuable insights, make
+            to unlock valuable insights
           </p>
           <a className="flex box-shadow gap-4 text-[20px] font-bold px-10 py-3 bg-[#703EDC] text-white rounded-full my-2">
             Discover
-            <img src={BtnStarted2}  alt="arrow" />
+            <img src={BtnStarted2} alt="arrow" />
           </a>
           <div className="ImageTarget">
             <img src={Layer_1} alt="targetImage" />
