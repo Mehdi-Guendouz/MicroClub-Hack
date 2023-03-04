f2632e8abce13cb35d00905197428d4a2c590740
brfft <belkacem.yerfa@gmail.com>
2023-03-04 11:33:39 +0100

    finale product deployed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
           <ul className="flex items-center justify-center h-full gap-16 ">
             {PagesLinks.map((pageLink) => (
               <li className=" text-[24px] font-medium " key={pageLink}>
-                <a href={`#${pageLink}`} >{pageLink}</a>
+                <a href={`#${pageLink}`}>{pageLink}</a>
                 <div className="Line"></div>
               </li>
             ))}
@@ -22,10 +22,10 @@ export const Navbar = () => {
         </div>
 
         <div className="flex items-center justify-center box-shadow  py-4 px-5 rounded-full bg-white box-shadow-btn text-[#703edc] font-bold text-[20px] ">
-          <a className="flex gap-3 ">
+          <Link to="/signUp" className="flex gap-3 ">
             Get Started
             <img className="ImageBtn" src={BtnStarted} alt="" />
-          </a>
+          </Link>
         </div>
       </nav>
     </header>
