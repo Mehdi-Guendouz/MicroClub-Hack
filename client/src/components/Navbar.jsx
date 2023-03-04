935f2692ff2701e81f690b94189d009daf839e93
abdoude12 <deabdou55@gmail.com>
2023-03-04 09:21:40 +0100

    the landing page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,8 +21,8 @@ export const Navbar = () => {
           </ul>
         </div>
 
-        <div className="flex items-center justify-center  py-4 px-5 rounded-full bg-white box-shadow-btn text-[#703edc] font-bold text-[20px] ">
-          <a className="flex gap-3">
+        <div className="flex items-center justify-center box-shadow  py-4 px-5 rounded-full bg-white box-shadow-btn text-[#703edc] font-bold text-[20px] ">
+          <a className="flex gap-3 ">
             Get Started
             <img className="ImageBtn" src={BtnStarted} alt="" />
           </a>
--- client/src/pages/Features.jsx
+++ client/src/pages/Features.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React from "react";
+import featureone from "../assets/one.png";
+import featuretwo from "../assets/two.png";
+import featurethree from "../assets/three.png";
+import arrows from '../assets/arrows.svg' ; 
 
 export const Features = () => {
-  return <div className="bg-[#703EDC]">
-    <div className='max-w-[1360px] m-auto'>
-      <h2>Out Features</h2>
-      
+  return (
+    <div className="bg-[#703EDC] mt-[3rem] relative py-[5rem]">
+      <img className="absolute top-[-14%] w-[14%]" src={arrows} />
+      <div className="max-w-[1360px] m-auto flex flex-col gap-[3.5rem]">
+        <h2 className="text-4xl text-center font-bold">Our Features</h2>
+        <div className="flex justify-between text-white text-xl text-center">
+          <div className="basis-[28%] flex flex-col gap-9">
+            <div>
+              <img src={featureone} />
+            </div>
+            <div>
+              <p>
+                Our web app provides an easy-to-use platform to manage your
+                business data, visualize it in various formats, and analyse it
+                to extract insights ."
+              </p>
+            </div>
+          </div>
+          <div className="basis-[28%]  flex flex-col gap-5">
+            <div>
+              <p>
+                Our web app leverages the power of artificial intelligence to
+                provide you with intelligent recommendations and predictions
+                that can help you optimize your business
+              </p>
+            </div>
+            <div>
+              <img src={featuretwo} />
+            </div>
+          </div>
+          <div className="basis-[28%]  flex flex-col gap-5">
+            <div>
+              <img src={featurethree} />
+            </div>
+            <div>
+              <p>
+                By combining data management, visualization, and AI
+                recommendations and predictions, our web app is designed to help
+                you optimize your business in every way possible.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>;
-}
+  );
+};
